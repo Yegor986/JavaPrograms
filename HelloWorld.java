23079f222b14e4fc8f9d1c8fc8612e79498ffa88
Yegor986 <sirotae1986@gmail.com>
2021-03-29 23:19:44 +0300

    29.03

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@ public class HelloWorld {
 public static void main (String [] args){
 	System.out.println ("Hello, World !!!")
 	System.out.println("HI");
+	for (int i = 0; i <10;i++){
+	System.out.print (i);
+	}
 }
 }
